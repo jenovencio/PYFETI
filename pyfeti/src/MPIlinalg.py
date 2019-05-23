37226415892d75b52e61ae1388899ecc632160b8
Guilherme Jenovencio <guilherme.jenovencio@tum.de>
2019-05-23 08:38:41 -0700

    new global dict communication using allgather

--- pyfeti/src/MPIlinalg.py
+++ pyfeti/src/MPIlinalg.py
@@ -54,16 +54,17 @@ def exchange_info(local_var,sub_id,nei_id,tag_id=15,isnumpy=False):
 
 def exchange_global_dict(local_dict,local_id,partitions_list):
     
-    logging.debug('Init exchange_global_dict')
-    logging.debug(('local_dict =' ,local_dict))
-    for global_id in partitions_list:
-        if global_id!=local_id:
-            nei_dict =  exchange_info(local_dict,local_id,global_id)
-            if nei_dict:
-                local_dict.update(nei_dict)
-
-    logging.debug('End exchange_global_dict')
-    return local_dict
+    list_of_dicts = comm.allgather(local_dict)
+    logging.info(list_of_dicts)
+    
+    global_dict = {}
+    for item in list_of_dicts:
+        try:
+            global_dict.update(item)
+        except:
+            continue
+    
+    return global_dict
 
 def exchange_global_dict_of_arrays(local_dict,local_id,partitions_list):
     
